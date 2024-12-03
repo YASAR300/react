2e5ec5bb2c914f2dcc49f2cbc87b196cd65c05d1
YASAR300 <everythingdecoding@gmail.com>
2024-12-03 15:12:40 +0530

    react

---
+++ vite_app/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://13mg.netlify.app/" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 100)}>
+          Count is {count}
+        </button>
+        <button 
+          onMouseEnter={() => setCount((count) => count + 200)}
+        >
+          Hover to add 200
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to open my website
+      </p>
+    </>
+  );
+}
+
+export default App;
